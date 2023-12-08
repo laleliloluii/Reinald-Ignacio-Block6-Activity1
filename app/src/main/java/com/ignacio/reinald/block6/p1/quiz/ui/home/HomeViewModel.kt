ac0459c6737f96a70fb08768606514888e76dbeb
Reinald Louie Ignacio <reinaldlouieignacio@gmail.com>
2023-12-08 18:02:18 +0800

    4th

---
+++ app/src/main/java/com/ignacio/reinald/block6/p1/quiz/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.ignacio.reinald.block6.p1.quiz.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/ignacio/reinald/block6/p1/quiz/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.ignacio.reinald.block6.p1.quiz.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
